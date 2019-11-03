7d415dd0e4282067f5c202a47f4218bdbc5eea8c
Aaron Jense <aaron.jense@protonmail.com>
2019-11-03 02:45:32 -0700

    Update Logger setup routine

--- Software/IAQ_Logger.py
+++ Software/IAQ_Logger.py
@@ -51,35 +51,74 @@ class Logger:
     A4 = None
     A5 = None
 
+    shutdown = False
 
     def __init__(self,setup_path=None):
+        # Initialize All Sub-Subsystems 
+        # Exit with Critical Error on Failure
+        if self._powerOn() == False: return self._powerOff()
+
+        self._printBanner("Setting Up Logger...")
+        # Verify that file exists from path given
+        status = self._verifySetupFile(setup_path)
+        if (status == False): return
+        # Initialize logger based on setup_path specifications
+        status = self._initFromFile(self.setup_path)
+        if status == True:
+            self._printBanner("Setup SUCCESS")
+        else:
+            self._printBanner("Setup FAILURE")
+
+    def _powerOn(self):
+        self.gui = GUI()
+        self.csv = FileHandler()
+        return True
+
+    def _powerOff(self):
+        print("Fatal Error: Failed to initialze sub-systems")
+        self.shutdown = True
+        return
+
+    def _verifySetupFile(self,setup_path=None):
         if (setup_path == None):
             setup_path = self.DEFAULT_PATH
-        self.gui = GUI()
         self.setup_path = setup_path
-        self.csv = FileHandler()
-        self._initFromFile(setup_path)
+        try:
+            open(self.setup_path, 'r')
+        except FileNotFoundError:
+            self._printBanner("Error: Setup File Not Found.")
+            return False
+        return True
 
     def _initFromFile(self,filepath=None):
-        self._printBanner("Setting Up Logger...")
-        setup_config = self.csv.csvToDict(filepath)
-        self.logger_id   = str(setup_config.get('Id',None)[0])
+        try:
+            setup_config = self.csv.csvToDict(filepath)
+            self.logger_id   = str(setup_config.get('Id',None)[0])
+        except TypeError:
+            self._printBanner("Fatal Error: Invalid Logger Setup file")
+            return
         self.logger_type = str(setup_config.get('Logger Type',None)[0])
         self.softwareVersion = str(setup_config.get('Software Version',None)[0])
         sensor_path  = setup_config.get('Sensors',None)
         sensors = self.csv.csvToDict(sensor_path)
         for s in sensors:
             self.sensor_names.append(s.get("Name",None))
-        self.setup_info = [self.logger_type,self.logger_id,self.softwareVersion,self.setup_path,self.sensor_names]
-        self._printSetupStatus()
-        self._printBanner("Setup Complete.")
+            self.setup_info = [self.logger_type,self.logger_id,
+                    self.softwareVersion,self.setup_path,self.sensor_names]
+
+        return self._printSetupStatus()
 
     def updateGui(self):
         self.gui.update_idletasks()
         self.gui.update()
 
     def printSystem(self,msg):
-        self.gui.startpage_output(msg)
+        try:
+            self.gui.startpage_output(msg)
+            return True
+        except AttributeError:
+            print("Fatal Error: GUI failed to load.")
+            return False
 
     def _printBanner(self,midText=None):
         hashes = "##################################################\n"
@@ -89,13 +128,28 @@ class Logger:
         self.printSystem(banner)
 
     def _printSetupStatus(self):
-        for info,text in zip(self.setup_info,self.setup_header):
-            info = text + ": " + str(info)
-            self.printSystem(info)
+        try:
+            for info,text in zip(self.setup_info,self.setup_header):
+                if info == None:
+                    return False
+                info = text + ": " + str(info)
+                self.printSystem(info)
+        except TypeError:
+            if self.setup_info == None:
+                self.printSystem("Error: Invalid Setup Info.")
+            elif self.setup_header == None:
+                self.printSystem("Error: Invalid Setup Header.")
+            return False
+        return True
+
+    def On(self):
+        if self.shutdown == True:
+            return False
+        return True
 
 logger = Logger()
 start = time.time()
-while True:
+while logger.On():
     logger.updateGui()
     end = time.time()
     if ((end - start) >= 1):
